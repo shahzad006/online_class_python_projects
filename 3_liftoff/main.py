c046a72db1dd44e130ff42fb4148682e26c3b60d
M Shahzad <muhammadshahzad45435@gmail.com>
2025-02-14 15:54:45 +0500

    first comment

---
+++ 3_liftoff/main.py
@@ -0,0 +1,27 @@
+import time
+
+
+for i in range(10,0,-1):
+    print(i)
+
+    time.sleep(1)
+
+print("Liftoff!")
+
+
+
+
+
+
+'''
+
+num = 10
+
+while num >=1:
+    print(num)
+    num -=1
+
+    if num == 0:
+        print("Lift Off!")
+
+ '''       
